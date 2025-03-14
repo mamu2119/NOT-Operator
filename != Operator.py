abf5c98fbaa52b9ffc699c186dbf811af0a62d83
mamu2119 <chaitanyapandya1985@gmail.com>
2025-03-14 21:26:25 +0530

    Add files via upload

---
+++ != Operator.py
@@ -0,0 +1,16 @@
+a = 54
+b = 56
+c = 56
+print(a != b)
+print(b != c)
+a = "python"
+b = "language"
+if a != b:
+    print(a, "and", b , "are different/not equal")
+a = 4
+b = 5
+if (a == 1) != (b == 5):
+    print("Hello")
+g = 55
+if (g%2 != 0):
+    print(g, "is not even number.")
